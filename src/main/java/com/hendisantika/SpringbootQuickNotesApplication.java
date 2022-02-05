c77496e7901c5bb472c3084ea2e4977c499346be
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-06 06:59:38 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringbootQuickNotesApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQuickNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootQuickNotesApplication.class, args);
+    }
+
+}
