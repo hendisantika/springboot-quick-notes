00ef65b0b01ffdc466f463d79bab1fa75cdadcff
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-06 07:12:14 +0700

    #13 - Add another method ib FindNoteDTO

--- src/main/java/com/hendisantika/dto/FindNoteDTO.java
+++ src/main/java/com/hendisantika/dto/FindNoteDTO.java
@@ -1,5 +1,6 @@
 package com.hendisantika.dto;
 
+import com.hendisantika.domain.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,4 +25,23 @@ public class FindNoteDTO {
 
     private String tag;
 
+    public boolean findAllTags() {
+        return tag.equals(Tag.FIND_ALL_TAGS);
+    }
+
+    public boolean noCriteria() {
+        return findAllTags() && title.equals("") && description.equals("");
+    }
+
+    public boolean searchByTitleOnly() {
+        return !title.equals("") && description.equals("");
+    }
+
+    public boolean searchByDescriptionOnly() {
+        return !description.equals("") && title.equals("");
+    }
+
+    public boolean searchByTagOnly() {
+        return description.equals("") && title.equals("");
+    }
 }
