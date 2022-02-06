66357342bfdf1aaa685c72c12afa7f494bc8cef2
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-06 07:24:23 +0700

    #30 - Add processAddTagFrom in TagController

--- src/main/java/com/hendisantika/controller/TagController.java
+++ src/main/java/com/hendisantika/controller/TagController.java
@@ -1,12 +1,19 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.domain.Tag;
 import com.hendisantika.dto.TagDTO;
 import com.hendisantika.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -38,4 +45,23 @@ public class TagController {
         return TAG_PAGE_NAME;
     }
 
+    @PostMapping("/add")
+    public String processAddTagFrom(@Valid @ModelAttribute(name = "tag") TagDTO tagDTO,
+                                    BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            return TAG_PAGE_NAME;
+        }
+
+        if (tagService.isTagInDatabase(tagDTO.getName())) {
+            redirectAttributes.addFlashAttribute("tagError",
+                    String.format("Tag %s already in database (letters size is ignored)", tagDTO.getName()));
+            return "redirect:/tag/add";
+        }
+
+        tagService.insertTagIntoDatabase(new Tag(tagDTO));
+        redirectAttributes.addFlashAttribute("tagCreated", true);
+
+        return "redirect:/home";
+    }
 }
