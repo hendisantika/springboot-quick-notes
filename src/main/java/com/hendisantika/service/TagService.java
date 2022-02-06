42554041e49d4b6bcc229a43ad7fef540680c0dd
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-06 07:14:22 +0700

    #17 - Add TagService

---
+++ src/main/java/com/hendisantika/service/TagService.java
@@ -0,0 +1,25 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-quick-notes
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/02/22
+ * Time: 07.13
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class TagService {
+    private final TagRepository tagRepository;
+
+    @Autowired
+    public TagService(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+}
