fcd8c77e375e8f883abfde61c0e141527d9dc155
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-06 07:20:05 +0700

    #22 - Add NotesController

---
+++ src/main/java/com/hendisantika/controller/NotesController.java
@@ -0,0 +1,35 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.service.NoteService;
+import com.hendisantika.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-quick-notes
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/02/22
+ * Time: 07.19
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@RequestMapping("/note")
+public class NotesController {
+
+    private static final String ADD_NOTE_PAGE_NAME = "add_note";
+    private static final String FIND_NOTE_PAGE_NAME = "find_note";
+
+    private final NoteService noteService;
+    private final TagService tagService;
+
+    @Autowired
+    public NotesController(NoteService noteService, TagService tagService) {
+        this.noteService = noteService;
+        this.tagService = tagService;
+    }
+
+}
